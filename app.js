dda9b91433acec027772a1c7ba703129fb8b64d3
Jason Fleagle <jasonfleagle@hotmail.com>
2022-01-15 13:00:39 +0000

    added in machine sate

---
+++ app.js
@@ -0,0 +1,17 @@
+let lotion = require('lotion')
+
+let app = lotion({
+	initialState: {
+		count: 0
+	}
+})
+
+app.use(function(state, tx) {
+	if (state.count === tx.nonce) {
+		state.count++
+	}
+})
+
+app.start().then(function(appInfo) {
+	console.log(`app started. gci: ${appInfo.GCI}`)
+})
